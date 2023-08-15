ecd8b0f3ffdaaa6184f864666ce6209ecc1ae81e
BatStak <baptistemenadier@proton.me>
2023-08-15 11:38:42 +0200

    Remove unused files and comments

--- package.sh
+++ package.sh
@@ -1,11 +1,5 @@
 # !/bin/bash
 
-# This script will pack everything into BanChecker_BROWSER_{VERSION}.zip ready for
-# uploading to the Chrome Store and Add-ons for Firefox. This way we can use unpacked extension while developing without changing
-# any filename or manifest.json or manually replacing files.
-
-# Use "Bash on Ubuntu on Windows" if you're on Windows 10 or cygwin to run on Windows.
-
 VERSION=$(grep '"version":' manifest_chrome.json | sed 's/^.*: //;s/"//g' | tr -d ',\r\n');
 echo "Ban Checker version in manifest.json: $VERSION. This script will pack everything into output folder";
 

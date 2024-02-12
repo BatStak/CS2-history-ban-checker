3b4cdd52c2695340f358cfa6efce654c1fd48331
Baptiste MENADIER <baptistemenadier@proton.me>
2024-02-12 18:57:28 +0100

    add template test for ban-statistics logic

--- src/app/components/ban-statistics/ban-statistics.component.spec.ts
+++ src/app/components/ban-statistics/ban-statistics.component.spec.ts
@@ -8,6 +8,7 @@ describe('BanStatisticsComponent', () => {
   let component: BanStatisticsComponent;
   let dataService: DataService;
   let fixture: ComponentFixture<BanStatisticsComponent>;
+  let dom: HTMLElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -17,11 +18,29 @@ describe('BanStatisticsComponent', () => {
     fixture = TestBed.createComponent(BanStatisticsComponent);
     component = fixture.componentInstance;
     dataService = fixture.debugElement.injector.get(DataService);
+    dom = fixture.nativeElement;
+  });
+
+  it('Test template no banned player', () => {
+    dataService.playersBannedAfter = [];
+
+    component._update();
+    fixture.detectChanges();
+    expect(dom.textContent).toContain('No banned player');
+
+    dataService.playersBannedAfter.push({
+      matches: [],
+      steamID64: 'test',
+    });
+    component._update();
+    fixture.detectChanges();
+    expect(dom.textContent).not.toContain('No banned player');
+    expect(dom.textContent).toContain(
+      '1 have been banned after playing with you'
+    );
   });
 
   it('Test update', () => {
-    const comp = new BanStatisticsComponent(dataService);
-
     // we add 100 players in database
     dataService.players = [];
     for (let i = 0; i < 100; i++) {
@@ -49,41 +68,41 @@ describe('BanStatisticsComponent', () => {
     }
 
     // 10% of players, 0 match concerned
-    comp._update();
-    expect(comp.playersCount).toEqual(100);
-    expect(comp.matchesCount).toEqual(100);
-    expect(comp.bannedCount).toEqual(10);
-    expect(comp.matchesConcerned).toEqual(0);
-    expect(comp.bannedPourcentage).toEqual(10);
-    expect(comp.matchPourcentage).toEqual(0);
+    component._update();
+    expect(component.playersCount).toEqual(100);
+    expect(component.matchesCount).toEqual(100);
+    expect(component.bannedCount).toEqual(10);
+    expect(component.matchesConcerned).toEqual(0);
+    expect(component.bannedPourcentage).toEqual(10);
+    expect(component.matchPourcentage).toEqual(0);
 
     // 1 match concerned (1%) of 100 matches
     dataService.matches[0].playersSteamID64.push('steamID0');
-    comp._update();
-    expect(comp.playersCount).toEqual(100);
-    expect(comp.matchesCount).toEqual(100);
-    expect(comp.bannedCount).toEqual(10);
-    expect(comp.matchesConcerned).toEqual(1);
-    expect(comp.matchPourcentage).toEqual(1);
+    component._update();
+    expect(component.playersCount).toEqual(100);
+    expect(component.matchesCount).toEqual(100);
+    expect(component.bannedCount).toEqual(10);
+    expect(component.matchesConcerned).toEqual(1);
+    expect(component.matchPourcentage).toEqual(1);
 
     // 3 match concerned (6%) of 50 matches
     dataService.matches[1].playersSteamID64.push('steamID0');
     dataService.matches[2].playersSteamID64.push('steamID0');
     dataService.matches.splice(50, 50);
-    comp._update();
-    expect(comp.playersCount).toEqual(100);
-    expect(comp.matchesCount).toEqual(50);
-    expect(comp.bannedCount).toEqual(10);
-    expect(comp.matchesConcerned).toEqual(3);
-    expect(comp.matchPourcentage).toEqual(6);
+    component._update();
+    expect(component.playersCount).toEqual(100);
+    expect(component.matchesCount).toEqual(50);
+    expect(component.bannedCount).toEqual(10);
+    expect(component.matchesConcerned).toEqual(3);
+    expect(component.matchPourcentage).toEqual(6);
 
     // 10 players out of 50 (20%)
     dataService.players.splice(50, 50);
-    comp._update();
-    expect(comp.playersCount).toEqual(50);
-    expect(comp.matchesCount).toEqual(50);
-    expect(comp.bannedCount).toEqual(10);
-    expect(comp.bannedPourcentage).toEqual(20);
+    component._update();
+    expect(component.playersCount).toEqual(50);
+    expect(component.matchesCount).toEqual(50);
+    expect(component.bannedCount).toEqual(10);
+    expect(component.bannedPourcentage).toEqual(20);
 
     // we now have 1050 players and 10 banned players (0.95%)
     for (let i = 0; i < 1000; i++) {
@@ -92,11 +111,11 @@ describe('BanStatisticsComponent', () => {
         steamID64: `steamID-new-${i}`,
       });
     }
-    comp._update();
-    expect(comp.playersCount).toEqual(1050);
-    expect(comp.matchesCount).toEqual(50);
-    expect(comp.bannedCount).toEqual(10);
-    expect(comp.bannedPourcentage).toEqual(0.95);
+    component._update();
+    expect(component.playersCount).toEqual(1050);
+    expect(component.matchesCount).toEqual(50);
+    expect(component.bannedCount).toEqual(10);
+    expect(component.bannedPourcentage).toEqual(0.95);
 
     // we now have 968 matches and 10 banned players (0.95%)
     for (let i = 0; i < 918; i++) {
@@ -104,11 +123,11 @@ describe('BanStatisticsComponent', () => {
         playersSteamID64: [],
       });
     }
-    comp._update();
-    expect(comp.playersCount).toEqual(1050);
-    expect(comp.matchesCount).toEqual(968);
-    expect(comp.bannedCount).toEqual(10);
-    expect(comp.matchesConcerned).toEqual(3);
-    expect(comp.matchPourcentage).toEqual(0.31);
+    component._update();
+    expect(component.playersCount).toEqual(1050);
+    expect(component.matchesCount).toEqual(968);
+    expect(component.bannedCount).toEqual(10);
+    expect(component.matchesConcerned).toEqual(3);
+    expect(component.matchPourcentage).toEqual(0.31);
   });
 });

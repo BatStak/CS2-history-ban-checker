39951d28b778171b1a068e495abea5937615d817
BatStak <baptistemenadier@proton.me>
2026-04-18 23:22:28 +0200

    rename project

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 npm run build;
-cp ./assets/* ./dist/cs-history-ban-checker/browser/
-mkdir ./dist/cs-history-ban-checker/chrome/
-mkdir ./dist/cs-history-ban-checker/firefox/
-cp -r ./dist/cs-history-ban-checker/browser/* ./dist/cs-history-ban-checker/chrome/
-cp -r ./dist/cs-history-ban-checker/browser/* ./dist/cs-history-ban-checker/firefox/
-mv ./dist/cs-history-ban-checker/chrome/manifest_chrome.json ./dist/cs-history-ban-checker/chrome/manifest.json
-rm ./dist/cs-history-ban-checker/chrome/manifest_firefox.json
-mv ./dist/cs-history-ban-checker/firefox/manifest_firefox.json ./dist/cs-history-ban-checker/firefox/manifest.json
-rm ./dist/cs-history-ban-checker/chrome/index.html ./dist/cs-history-ban-checker/chrome/favicon.ico
-rm ./dist/cs-history-ban-checker/firefox/index.html ./dist/cs-history-ban-checker/firefox/favicon.ico
-cd ./dist/cs-history-ban-checker/chrome/ && zip ../chrome.zip ./* && cd -
-cd ./dist/cs-history-ban-checker/firefox/ && zip ../firefox.zip ./* && cd -
+cp ./assets/* ./dist/cs2-ban-checker/browser/
+mkdir ./dist/cs2-ban-checker/chrome/
+mkdir ./dist/cs2-ban-checker/firefox/
+cp -r ./dist/cs2-ban-checker/browser/* ./dist/cs2-ban-checker/chrome/
+cp -r ./dist/cs2-ban-checker/browser/* ./dist/cs2-ban-checker/firefox/
+mv ./dist/cs2-ban-checker/chrome/manifest_chrome.json ./dist/cs2-ban-checker/chrome/manifest.json
+rm ./dist/cs2-ban-checker/chrome/manifest_firefox.json
+mv ./dist/cs2-ban-checker/firefox/manifest_firefox.json ./dist/cs2-ban-checker/firefox/manifest.json
+rm ./dist/cs2-ban-checker/chrome/index.html ./dist/cs2-ban-checker/chrome/favicon.ico
+rm ./dist/cs2-ban-checker/firefox/index.html ./dist/cs2-ban-checker/firefox/favicon.ico
+cd ./dist/cs2-ban-checker/chrome/ && zip ../chrome.zip ./* && cd -
+cd ./dist/cs2-ban-checker/firefox/ && zip ../firefox.zip ./* && cd -

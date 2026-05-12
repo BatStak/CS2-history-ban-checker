e15aa770c6d18b1b05269bc1e196fbda7871dca5
BatStak <baptistemenadier@proton.me>
2026-05-12 10:36:58 +0200

    run npm audit fix + add delay on profile prepend

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,7 @@ const app = document.createElement("cs2-history-app-root");
 const profile = document.querySelector(".profile_page .profile_content .profile_leftcol");
 const gcpd = document.querySelector("#subtabs");
 if (profile) {
-    profile.prepend(app);
+    setTimeout(() => profile.prepend(app), 500);
 } else if (gcpd) {
     gcpd.after(app);
 }

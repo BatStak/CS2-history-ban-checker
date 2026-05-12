0659e9fa7cacb794357e74e1bbd1cc607fd03bad
BatStak <baptistemenadier@proton.me>
2026-05-12 11:52:45 +0200

    fix: broken version

--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,8 @@
 const app = document.createElement("cs2-history-app-root");
-const profile = document.querySelector(".profile_page .profile_content .profile_leftcol");
+const profile = document.querySelector(".profile_header_bg");
 const gcpd = document.querySelector("#subtabs");
 if (profile) {
-    setTimeout(() => profile.prepend(app), 500);
+    profile.append(app);
 } else if (gcpd) {
     gcpd.after(app);
 }
